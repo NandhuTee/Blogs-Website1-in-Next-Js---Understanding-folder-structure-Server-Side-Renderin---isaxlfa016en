48d83dbb473f6f3198f70e3e8eddc6f674b7d79d
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-04-26 12:15:11 +0530

    Update page.js

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useParams } from 'next/navigation';
 
-export default function PostDetail({ match }) {
-  // Complete the function
+export default function PostDetail() {
+    const { id } = useParams();
+
+    return (
+        <div>
+            <h1>Post Detail: {id}</h1>
+        </div>
+    );
 }
